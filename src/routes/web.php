9a4376d8b9858d744f0b4677f522272e13e7f56d
Santiago González Rojo <santiagogonzalezrojo@yandex.ru>
2026-03-13 20:46:14 -0300

    Added Breeze and spatie dependencies

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,26 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductionsController;
-use App\Http\Controllers\CalendarController;
-use App\Http\Controllers\ReservationsController;
-use App\Http\Controllers\ActivitiesController;
 
-// Home/Park routes
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/el-parque', [HomeController::class, 'park'])->name('park');
-Route::get('/otros-parques', [HomeController::class, 'otherParks'])->name('other.parks');
-Route::get('/contacto', [HomeController::class, 'contact'])->name('contact');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Productions routes
-Route::get('/producciones', [ProductionsController::class, 'index'])->name('productions.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Calendar routes
-Route::get('/calendario-de-uso', [CalendarController::class, 'index'])->name('calendar.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Reservations routes
-Route::get('/reservas', [ReservationsController::class, 'index'])->name('reservations.index');
-
-// Activities routes
-Route::get('/actividades', [ActivitiesController::class, 'index'])->name('activities.index');
+require __DIR__.'/auth.php';
